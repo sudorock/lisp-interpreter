72836390e9ac85cdfbbf3de9826852189deb23aa
Sunil KS <kslvsunil@gmail.com>
2019-10-26 22:42:23 +0530

    repeat fully working

--- src/lisp_interpreter/core.clj
+++ src/lisp_interpreter/core.clj
@@ -1,39 +1,42 @@
-(ns lisp-interpreter.core (:require [clojure.string :refer [split trim replace-first join escape]]))
+(ns lisp-interpreter.core
+  (:require [clojure.string :refer [split trim replace-first join escape]]))
 (declare parse evaluate)
 
 (defn throw-error [] (throw (Exception. "Parse Error")))
 (defn uuid [] (str (java.util.UUID/randomUUID)))
-(def const {"+"  #(apply + %), "-" #(apply - %), "*" #(apply * %), "/" #(apply / %), ">" #(apply > %), "<" #(apply < %),
-            ">=" #(apply >= %), "<=" #(apply <= %), "="  #(apply = %),  "not" #(apply not %), "abs" (fn [a] (apply #(max % (- %)) a)),
-            "max" #(apply max %), "min" #(apply min %), "sqrt" (fn [a] (apply #(Math/sqrt %) a)), "true" true, "false" false})
 
 (def envs (atom {:global {}}))
 (def parent (atom {}))
+(def const {"+"   #(apply + %), "-" #(apply - %), "*" #(apply * %), "/" #(apply / %), ">" #(apply > %), "<" #(apply < %),
+            ">="  #(apply >= %), "<=" #(apply <= %), "=" #(apply = %), "not" #(apply not %), "max" #(apply max %),
+            "min" #(apply min %), "sqrt" (fn [a] (apply #(Math/sqrt %) a)), "true" true, "false" false,
+            "abs" (fn [a] (apply #(max % (- %)) a)), "pi" 3.141592653589793})
 
 (defn env-find [v env] (when-let [cur (@envs env)] (if-let [vl (cur v)] vl (env-find v (@parent env)))))
 
 (defn atomize [el]
   (try (Integer/parseInt el)
-       (catch Exception e
-         (try (Double/parseDouble el)
-              (catch Exception e (if-let [cnst (get const el)] cnst el))))))
+       (catch Exception e (try (Double/parseDouble el)
+                               (catch Exception e (if-let [cnst (get const el)] cnst el))))))
 
-(defn handle-lambda [form env]
+(defn handle-lambda [exp env]
   (fn [args]
     (let [cur-env (uuid)]
+      (println exp)
+      (println args)
       (swap! parent assoc cur-env env)
-      (swap! envs assoc cur-env (zipmap (form 1) args))
-      (evaluate (form 2) cur-env))))
+      (swap! envs assoc cur-env (zipmap (exp 1) args))
+      (evaluate (exp 2) cur-env))))
 
-(defn evaluate [form env]
+(defn evaluate [exp env]
   (cond
-    (or (boolean? form) (number? form)) form
-    (string? form) (env-find form env)
-    (= (form 0) "define") (swap! envs assoc-in [env (form 1)] (evaluate (form 2) env))
-    (= (form 0) "if") (if (evaluate (form 1) env) (evaluate (form 2) env) (evaluate (form 3) env))
-    (= (form 0) "lambda") (handle-lambda form env)
-    (fn? (env-find (form 0) env)) ((env-find (form 0) env) (map #(evaluate % env) (subvec form 1)))
-    :else ((form 0) (map #(evaluate % env) (subvec form 1)))))
+    (or (boolean? exp) (number? exp) (fn? exp)) exp
+    (string? exp) (env-find exp env)
+    (= (exp 0) "define") (swap! envs assoc-in [env (exp 1)] (evaluate (exp 2) env))
+    (= (exp 0) "if") (if (evaluate (exp 1) env) (evaluate (exp 2) env) (evaluate (exp 3) env))
+    (= (exp 0) "lambda") (handle-lambda exp env)
+    (fn? (env-find (exp 0) env)) ((env-find (exp 0) env) (map #(evaluate % env) (subvec exp 1)))
+    :else ((exp 0) (map #(evaluate % env) (subvec exp 1)))))
 
 (defn parse [s env lmda]
   (cond
