8130a46b52c7a35525e87c2006c3c6ac493e10f3
Sunil KS <kslvsunil@gmail.com>
2019-10-29 13:15:12 +0530

    map and some other functions added

--- src/lisp_interpreter/core.clj
+++ src/lisp_interpreter/core.clj
@@ -6,14 +6,13 @@
 
 (def envs (atom {:global {}}))
 (def parent (atom {}))
-(def const {"+"       #(apply + %), "-" #(apply - %), "*" #(apply * %), "/" #(apply / %), ">" #(apply > %), "<" #(apply < %),
-            ">="      #(apply >= %), "<=" #(apply <= %), "=" #(apply = %), "not" #(apply not %), "max" #(apply max %),
-            "min"     #(apply min %), "sqrt" (fn [a] (apply #(Math/sqrt %) a)), "expt" (fn [a] (apply #(Math/pow %1 %2) a)),
-            "round"   (fn [a] (apply #(Math/round %) a)), "abs" (fn [a] (apply #(max % (- %)) a)),
-            "number?" #(apply number? %), "procedure?" #(apply fn? %), "symbol?" #(apply string? %),
-            "car"     (fn [a] (apply #(first %) a)), "cdr" (fn [a] (apply #(rest %) a)),
-            "true"    true, "false" false,
-            "pi"      3.141592653589793})
+(def const {"+" #(apply + %) "-" #(apply - %) "*" #(apply * %) "/" #(apply / %) ">" #(apply > %) "<" #(apply < %)
+            ">=" #(apply >= %) "<=" #(apply <= %) "=" #(apply = %) "not" #(apply not %) "max" #(apply max %)
+            "min" #(apply min %) "sqrt" (fn [a] (apply #(Math/sqrt %) a)) "expt" (fn [a] (apply #(Math/pow %1 %2) a))
+            "apply" #(apply %1 %&) "list" identity "round"   (fn [a] (apply #(Math/round %) a)) "abs" (fn [a] (apply #(max % (- %)) a))
+            "number?" #(apply number? %) "procedure?" #(apply fn? %) "symbol?" #(apply string? %) "equal?" #(apply = %)
+            "car" #(apply first %) "cdr" #(apply next %) "cons" (fn [a] (apply #(cons %1 %2) a)) "pi" 3.141592653589793
+            "map" (fn [[f & bdy]] (map f (map vector (first bdy)))) "true" true "false" false})
 
 (defn env-find [sym env] (when-let [cur (@envs env)] (if-let [vl (cur sym)] vl (env-find sym (@parent env)))))
 
@@ -33,12 +32,13 @@
   (cond
     ((some-fn number? boolean? fn?) exp) exp
     (string? exp) (env-find exp env)
-    (or (= (exp 0) "define") (= (exp 0) "set!")) (swap! envs assoc-in [env (exp 1)] (evaluate (exp 2) env))
-    (= (exp 0) "if") (if (evaluate (exp 1) env) (evaluate (exp 2) env) (evaluate (exp 3) env))
-    (= (exp 0) "quote") (exp 1)
-    (= (exp 0) "lambda") (handle-lambda exp env)
-    (fn? (env-find (exp 0) env)) ((env-find (exp 0) env) (map #(evaluate % env) (subvec exp 1)))
-    :else ((evaluate (exp 0) env) (map #(evaluate % env) (subvec exp 1)))))
+    (some #(= (get exp 0) %) ["set!" "define"]) (swap! envs assoc-in [env (exp 1)] (evaluate (exp 2) env))
+    (= (get exp 0) "if") (if (evaluate (exp 1) env) (evaluate (exp 2) env) (evaluate (exp 3) env))
+    (= (get exp 0) "quote") (exp 1)
+    (= (get exp 0) "lambda") (handle-lambda exp env)
+    (fn? (env-find (get exp 0) env)) ((env-find (get exp 0) env) (map #(evaluate % env) (subvec exp 1)))
+    (fn? (get exp 0)) ((get exp 0) (map #(evaluate % env) (subvec exp 1)))
+    :else (if (coll? exp) exp (throw-error))))
 
 (defn parse [s eval?]
   (cond
@@ -52,63 +52,3 @@
     :else (if-let [res (re-find #"^\S*[^\(\)\s]+" s)] [(atomize (trim res)) (trim (subs s (count res)))] s)))
 
 (defn -main [s] (parse (trim s) true))
-
-;; quote
-;; apply, begin, car, cdr, cons, eq, equal?, length, list, list?, map, null?, print
-
-;(defn parse [s env parse-only?]
-;  (cond
-;    (re-find #"^\)" s) (throw-error)
-;    (re-find #"^\(" s) (loop [rst (trim (replace-first s #"^\(" "")), exp []]
-;                         (cond
-;                           (empty? rst) (throw-error)
-;                           (re-find #"^(?:lambda|quote)\s+" rst) (if parse-only?
-;                                                                   (recur (replace-first rst #"^(?:lambda|quote)\s+" "") (conj exp (trim (re-find #"^(?:lambda|quote)\s+" rst))))
-;                                                                   (let [[lexp rmn] (parse (str "(" rst) env true)] [lexp (trim rmn)]))
-;                           (re-find #"^\)" rst) [(if parse-only? exp (evaluate exp env)) (trim (replace-first rst #"^\)" ""))]
-;                           :else (when-let [[res rmn] (parse rst env parse-only?)] (recur (trim rmn) (conj exp res)))))
-;    :else (if-let [res (re-find #"^\S*[^\(\)\s]+" s)] [(atomize (trim res)) (trim (subs s (count res)))] s)))
-
-;(defn -main [s] (parse (trim s)))
-
-;(defn parse
-;  ([s cnt]
-;   (cond
-;     (re-find #"^\)" s) (throw-error)
-;     (re-find #"^\(\s*(?:lambda|quote)\s+" s) (parse s (inc cnt) true)
-;     (re-find #"^\(" s) (parse s 0 false)
-;     :else (if-let [res (re-find #"^\S*[^\(\)\s]+" s)] [(atomize (trim res)) (trim (subs s (count res)))] s)))
-;  ([s cnt parse-only?]
-;   (loop [rst (trim (replace-first s #"^\(" "")), exp [], cnt]
-;     (cond
-;       (empty? rst) (throw-error)
-;       (re-find #"^\)" rst) [(if parse-only? exp (evaluate exp :global)) (trim (replace-first rst #"^\)" ""))]
-;       :else (when-let [[res rmn] (parse rst cnt parse-only?)] (recur (trim rmn) (conj exp res)))))))
-
-;(defn parse [s env lm]
-;  (cond
-;    (re-find #"^\)" s) (throw-error)
-;    (re-find #"^\(" s) (loop [rst (trim (replace-first s #"^\(" "")), exp []]
-;                         (cond
-;                           (empty? rst) (throw-error)
-;                           (re-find #"^lambda\s+" rst) (if lm
-;                                                         (recur (replace-first rst #"^lambda\s+" "") (conj exp "lambda"))
-;                                                         (let [[lexp rmn] (parse (str "(" rst) env true)] [lexp (trim rmn)]))
-;                           (re-find #"^\)" rst) [(if lm exp (evaluate exp env)) (trim (replace-first rst #"^\)" ""))]
-;                           :else (when-let [[res rmn] (parse rst env lm)] (recur (trim rmn) (conj exp res)))))
-;    :else (if-let [res (re-find #"^\S*[^\(\)\s]+" s)] [(atomize (trim res)) (trim (subs s (count res)))] s)))
-
-;(defn parse [s parse-only?]
-;  (cond
-;    (re-find #"^\)" s) (throw-error)
-;    (re-find #"^\(" s) (loop [rst (trim (replace-first s #"^\(" "")), exp []]
-;                         (cond
-;                           (empty? rst) (throw-error)
-;                           (re-find #"^lambda\s+" rst) (if parse-only?
-;                                                         (recur (replace-first rst #"^lambda\s+" "") (conj exp "lambda"))
-;                                                         (let [[lexp rmn] (parse (str "(" rst) true)] [lexp (trim rmn)]))
-;                           (re-find #"^quote\s+" rst) (let [[res rmn] (parse (replace-first rst #"^quote\s+" "") true)]
-;                                                        [res (trim (replace-first rmn #"^\s*\)" ""))])
-;                           (re-find #"^\)" rst) [(if parse-only? exp (evaluate exp :global)) (trim (replace-first rst #"^\)" ""))]
-;                           :else (when-let [[res rmn] (parse rst parse-only?)] (recur (trim rmn) (conj exp res)))))
-;    :else (if-let [res (re-find #"^\S*[^\(\)\s]+" s)] [(atomize (trim res)) (trim (subs s (count res)))] s)))
