1f43e234077f26556b048ff5c7e4cf5f0babb82b
Sunil KS <kslvsunil@gmail.com>
2019-11-07 12:57:45 +0530

    macros working

--- src/lisp_interpreter/core.clj
+++ src/lisp_interpreter/core.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :refer [split trim replace-first join escape]]))
 (declare evaluate)
 
-(defn throw-error [] (throw (Exception. "Parse Error")))
+(defn throw-error [s] (.println *err* (format "ERROR: %s" s)))
 
 (def parent {})
 (def macros {})
@@ -56,63 +56,71 @@
                (def envs (assoc envs cur-env arg-map))
                (evaluate (exp 2) cur-env))))
 
-(defn is-pair [exp] (and (vector? exp) (not-empty exp)))
-
 (defn handle-quasi [exp]
-  (if (is-pair exp)
-    (cond
-      (= (exp 0) 'unqt) (exp 1)
-      (and (is-pair (exp 0)) (= ((get exp 0) 0) 'unqt-splice)) ['concat ((get exp 0) 1) (handle-quasi (subvec exp 1))]
-      :else ['cons (handle-quasi (exp 0)) (handle-quasi (subvec exp 1))])
-    ['quote exp]))
+  (letfn [(is-pair [exp] (and (vector? exp) (not-empty exp)))]
+    (if (is-pair exp)
+      (cond
+        (= (exp 0) 'unqt) (exp 1)
+        (and (is-pair (exp 0)) (= ((get exp 0) 0) 'unqt-splice)) ['concat ((get exp 0) 1) (handle-quasi (subvec exp 1))]
+        :else ['cons (handle-quasi (exp 0)) (handle-quasi (subvec exp 1))])
+      ['quote exp])))
 
-(defn evaluate [exp env]
-  (cond
-    ((some-fn number? boolean? fn? nil? #(contains? #{'if 'begin 'lambda 'define} %)) exp) exp
-    (symbol? exp) (env-find exp env)
-    (some #(= (get exp 0) %) ['set! 'define]) (def envs (assoc-in envs [env (exp 1)] (evaluate (exp 2) env)))
-    (= (get exp 0) 'if) (if (evaluate (exp 1) env) (evaluate (exp 2) env) (evaluate (get exp 3) env))
-    (= (get exp 0) 'quote) (exp 1)
-    (= (get exp 0) 'q-quote) (evaluate (handle-quasi (exp 1)) env)
-    (= (get exp 0) 'begin) (last (map #(evaluate % env) (subvec exp 1)))
-    (= (get exp 0) 'lambda) (handle-lambda exp env)
-    (= (get exp 0) 'define-macro) (def macros (assoc macros (exp 1) (evaluate (exp 2) env)))
-    (fn? (macros (get exp 0))) (evaluate ((macros (get exp 0)) (subvec exp 1)) env)
-    (fn? (env-find (get exp 0) env)) ((env-find (get exp 0) env) (map #(evaluate % env) (subvec exp 1)))
-    (fn? (get exp 0)) ((get exp 0) (map #(evaluate % env) (subvec exp 1)))
-    :else (if (coll? exp) exp (throw-error))))
+(defn evaluate
+  ([exp] (evaluate exp :g))
+  ([exp env] (if-let [op (if (vector? (get exp 0)) (evaluate (get exp 0) :g) (get exp 0))]
+               (cond
+                 (some #(= op %) ['set! 'define]) (def envs (assoc-in envs [env (exp 1)] (evaluate (exp 2) env)))
+                 (= op 'if) (if (evaluate (exp 1) env) (evaluate (exp 2) env) (evaluate (get exp 3) env))
+                 (= op 'quote) (exp 1)
+                 (= op 'q-quote) (evaluate (handle-quasi (exp 1)) env)
+                 (= op 'begin) (last (map #(evaluate % env) (subvec exp 1)))
+                 (= op 'lambda) (handle-lambda exp env)
+                 (= op 'def-macro) (def macros (assoc macros (exp 1) (evaluate (exp 2) env)))
+                 (fn? (macros op)) (evaluate ((macros op) (subvec exp 1)) env)
+                 (fn? (env-find op env)) ((env-find op env) (map #(evaluate % env) (subvec exp 1)))
+                 (fn? op) (op (map #(evaluate % env) (subvec exp 1)))
+                 :else (throw-error "Invalid Expression"))
+               (if-let [value (env-find exp env)]
+                 value
+                 (if ((some-fn number? boolean? fn? nil?) exp)
+                   exp
+                   (format "Unable to resolve symbol: %s" exp))))))
 
-(defn reader [s eval?]
+(defn reader [s]
   (cond
-    (re-find #"^\)" s) (throw-error)
-    (and eval? (re-find #"^\(\s*(?:lambda|quote|q-quote|define-macro|if|begin)\s+" s)) (let [[res rmn] (reader s false)] [(evaluate res :g) (trim rmn)])
+    (re-find #"^\)" s) (throw-error "Unmatched delimiter")
     (re-find #"^\(" s) (loop [rst (trim (replace-first s #"^\(" "")), exp []]
                          (cond
-                           (empty? rst) (throw-error)
-                           (re-find #"^\)" rst) [(if eval? (evaluate exp :g) exp) (trim (replace-first rst #"^\)" ""))]
-                           :else (when-let [[res rmn] (reader rst eval?)] (recur (trim rmn) (conj exp res)))))
+                           (empty? rst) (throw-error "EOF while reading")
+                           (re-find #"^\)" rst) [exp (trim (replace-first rst #"^\)" ""))]
+                           :else (when-let [[res rmn] (reader rst)] (recur (trim rmn) (conj exp res)))))
     :else (if-let [res (re-find #"^\S*[^\(\)\s]+" s)] [(atomize (trim res)) (trim (subs s (count res)))] s)))
 
 ;; Interpreter and REPL
-(defn interpret [s] (reader (trim s) true))
+(defn interpret [s]
+  (let [[ast rmn] (reader (trim s))]
+    (if (empty? rmn)
+      (evaluate ast :g)
+      (throw-error "Unmatched delimiter"))))
 
 (defn repl []
   (do (printf "\033[0;1mcljisp ~ \u03BB  \033[34;1m") (flush)
-      (let [[res rmn] (interpret (read-line))]
-        (if (empty? rmn) (do (printf "%s\n" (pr-str res)) (repl)) (throw-error)))))
+      (let [res (interpret (read-line))] (printf "%s\n" (pr-str res)) (repl))))
+
 (defn -main [] (repl))
 
 
-;"(define-macro when (lambda (cnd then) (list (quote if) cnd then nil)))"
-;"(define-macro when (lambda (cnd . then) (list (quote if) cnd then nil)))"
-;"(define-macro when (lambda (cnd . then) (list (quote if) cnd (cons (quote begin) then)  nil)))"
-
-;(define-macro when (lambda (cnd . then) (quasi-quote (if (unquote cnd) (begin (unquote-splice then)) nil))))
-
-;(define z (lambda (k l . els) (print k l els)))
 
 
-;(define-macro unless (lambda (cnd . branch) (list (quote if) (list (quote not) cnd) (cons (quote begin) branch))))
 
-;(define-macro my-or (lambda (x y) `(if ,x ,x ,y)))
+;"(def-macro when (lambda (k . l) (q-quote (if (unqt k) (begin (unqt-splice l)) ))))"
+
+
+
+
+
+
+
+
+
 
