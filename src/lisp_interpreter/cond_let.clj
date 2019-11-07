c2c07a4a8de59cda2f344d303c651f2af3cecc99
Sunil KS <kslvsunil@gmail.com>
2019-11-07 17:37:34 +0530

    refactored code with cond-let

---
+++ src/lisp_interpreter/cond_let.clj
@@ -0,0 +1,12 @@
+(ns lisp-interpreter.cond-let)
+
+(defmacro cond-let
+  ([] nil)
+  ([exp] (throw (IllegalArgumentException. "cond-let requires an even number of forms")))
+  ([cnd-bnd exp & rmn] (if (vector? cnd-bnd)
+                         (let [form (cnd-bnd 0) tst (cnd-bnd 1)]
+                           `(let [tmp# ~tst]
+                              (if tmp#
+                                (let [~form tmp#] ~exp)
+                                (cond-let ~@rmn))))
+                         `(if ~cnd-bnd ~exp (cond-let ~@rmn)))))
--- src/lisp_interpreter/core.clj
+++ src/lisp_interpreter/core.clj
@@ -1,42 +1,20 @@
 (ns lisp-interpreter.core
-  (:require [clojure.string :refer [split trim replace-first join escape]]))
-(declare evaluate)
+  (:require [clojure.string :refer [split trim replace-first join escape]]
+            [lisp-interpreter.cond-let :refer [cond-let]]))
+(declare evaluate -main)
 
 (defn throw-error [s] (.println *err* (format "ERROR: %s" s)))
 
 (def parent {})
 (def macros {})
-(def envs {:g {'+               #(apply + %)
-               '-               #(apply - %)
-               '*               #(apply * %)
-               '/               #(apply / %)
-               '>               #(apply > %)
-               '<               #(apply < %)
-               '>=              #(apply >= %)
-               '<=              #(apply <= %)
-               '=               #(apply = %)
-               'not             #(apply not %)
-               'max             #(apply max %)
-               'min             #(apply min %)
-               'sqrt            (fn [a] (apply #(Math/sqrt %) a))
-               'expt            (fn [a] (apply #(Math/pow %1 %2) a))
-               'round           (fn [a] (apply #(Math/round %) a))
-               'abs             (fn [a] (apply #(max % (- %)) a))
-               'number?         #(apply number? %)
-               'procedure?      #(apply fn? %)
-               'symbol?         #(apply string? %)
-               'equal?          #(apply = %)
-               'list            vec
-               'car             #(apply first %)
-               'cdr             #(apply next %)
-               'cons            (fn [a] (vec (apply #(cons %1 %2) a)))
-               'concat          #(vec (apply concat %))
-               'apply           #(apply %1 %&)
-               'map             (fn [[f & bdy]] (map f (map vector (first bdy))))
-               'print           #(apply prn %)
-               'pi              3.141592653589793
-               (symbol "true")  true
-               (symbol "false") false}})
+(def envs {:g {'+ #(apply + %) '- #(apply - %) '* #(apply * %) '/ #(apply / %) '>  #(apply > %) '<  #(apply < %)
+               '>= #(apply >= %) '<= #(apply <= %) '=  #(apply = %) 'not  #(apply not %) 'max  #(apply max %)
+               'min  #(apply min %) 'sqrt (fn [a] (apply #(Math/sqrt %) a)) 'expt (fn [a] (apply #(Math/pow %1 %2) a))
+               'round (fn [a] (apply #(Math/round %) a)) 'abs (fn [a] (apply #(max % (- %)) a)) 'number? #(apply number? %)
+               'procedure? #(apply fn? %) 'symbol?  #(apply string? %) 'equal? #(apply = %) 'list vec 'car #(apply first %)
+               'cdr #(apply next %) 'cons (fn [a] (vec (apply #(cons %1 %2) a))) 'concat  #(vec (apply concat %))
+               'apply  #(apply %1 %&) 'map (fn [[f & bdy]] (map f (map vector (first bdy))))'print  #(apply prn %)
+               'pi 3.141592653589793 (symbol "true") true (symbol "false") false}})
 
 (defn env-find [sym env] (when-let [cur (envs env)] (if-let [vl (cur sym)] vl (env-find sym (parent env)))))
 
@@ -67,10 +45,14 @@
         :else ['cons (handle-quasi (exp 0)) (handle-quasi (subvec exp 1))])
       ['quote exp])))
 
+(defn apply-fn [func arg-list env]
+  (try (func (map #(evaluate % env) arg-list))
+       (catch Exception e (throw-error "Invalid Form"))))
+
 (defn evaluate
   ([exp] (evaluate exp :g))
   ([exp env] (if-let [op (and (vector? exp) (if (vector? (get exp 0)) (evaluate (get exp 0) :g) (get exp 0)) )]
-               (cond
+               (cond-let
                  (some #(= op %) ['set! 'define]) (def envs (assoc-in envs [env (exp 1)] (evaluate (exp 2) env)))
                  (= op 'if) (if (evaluate (exp 1) env) (evaluate (exp 2) env) (evaluate (get exp 3) env))
                  (= op 'quote) (exp 1)
@@ -78,15 +60,13 @@
                  (= op 'begin) (last (map #(evaluate % env) (subvec exp 1)))
                  (= op 'lambda) (handle-lambda exp env)
                  (= op 'def-macro) (def macros (assoc macros (exp 1) (evaluate (exp 2) env)))
-                 (fn? (macros op)) (evaluate ((macros op) (subvec exp 1)) env)
-                 (fn? (env-find op env)) ((env-find op env) (map #(evaluate % env) (subvec exp 1)))
-                 (fn? op) (op (map #(evaluate % env) (subvec exp 1)))
-                 :else (throw-error "Invalid Expression"))
-               (if-let [value (env-find exp env)]
-                 value
-                 (if ((some-fn number? boolean? fn? nil? string?) exp)
-                   exp
-                   (format "Unable to resolve symbol: %s" exp))))))
+                 [macro (macros op)] (evaluate (macro (subvec exp 1)) env)
+                 [func (env-find op env)] (apply-fn func (subvec exp 1) env)
+                 :else (apply-fn op (subvec exp 1) env))
+               (cond-let
+                 [value (env-find exp env)] value
+                 ((some-fn number? boolean? fn? nil? string?) exp) exp
+                 :else (throw-error (format "Unable to resolve symbol: %s" exp))))))
 
 (defn reader [s]
   (cond
@@ -96,7 +76,7 @@
                            (empty? rst) (throw-error "EOF while reading")
                            (re-find #"^\)" rst) [exp (trim (replace-first rst #"^\)" ""))]
                            :else (when-let [[res rmn] (reader rst)] (recur (trim rmn) (conj exp res)))))
-    (re-find #"^['`~]\S+" s) (cond
+    (re-find #"^['`~]@?\S+" s) (cond
                                (re-find #"^'\S+" s) (let [[res rmn] (reader (subs s 1))] [['quote res] rmn])
                                (re-find #"^`\S+" s) (let [[res rmn] (reader (subs s 1))] [['q-quote res] rmn])
                                (re-find #"^~@\S+" s) (let [[res rmn] (reader (subs s 2))] [['unqt-splice res] rmn])
@@ -130,5 +110,24 @@
 
 
 
-
+;(defn evaluate
+;  ([exp] (evaluate exp :g))
+;  ([exp env] (if-let [op (and (vector? exp) (if (vector? (get exp 0)) (evaluate (get exp 0) :g) (get exp 0)) )]
+;               (cond
+;                 (some #(= op %) ['set! 'define]) (def envs (assoc-in envs [env (exp 1)] (evaluate (exp 2) env)))
+;                 (= op 'if) (if (evaluate (exp 1) env) (evaluate (exp 2) env) (evaluate (get exp 3) env))
+;                 (= op 'quote) (exp 1)
+;                 (= op 'q-quote) (evaluate (handle-quasi (exp 1)) env)
+;                 (= op 'begin) (last (map #(evaluate % env) (subvec exp 1)))
+;                 (= op 'lambda) (handle-lambda exp env)
+;                 (= op 'def-macro) (def macros (assoc macros (exp 1) (evaluate (exp 2) env)))
+;                 (fn? (macros op)) (evaluate ((macros op) (subvec exp 1)) env)
+;                 (fn? (env-find op env)) ((env-find op env) (map #(evaluate % env) (subvec exp 1)))
+;                 (fn? op) (op (map #(evaluate % env) (subvec exp 1)))
+;                 :else (throw-error "Invalid Expression"))
+;               (if-let [value (env-find exp env)]
+;                 value
+;                 (if ((some-fn number? boolean? fn? nil? string?) exp)
+;                   exp
+;                   (format "Unable to resolve symbol: %s" exp))))))
 
