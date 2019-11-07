c3fcb0d2b4e4d5794d6b1243f83ce962c5b072b6
Sunil KS <kslvsunil@gmail.com>
2019-11-07 08:40:24 +0530

    switching to reader separate

--- src/lisp_interpreter/core.clj
+++ src/lisp_interpreter/core.clj
@@ -57,11 +57,12 @@
                (evaluate (exp 2) cur-env))))
 
 (defn is-pair [exp] (and (vector? exp) (not-empty exp)))
+
 (defn handle-quasi [exp]
   (if (is-pair exp)
     (cond
-      (= (exp 0) 'unquote) (exp 1)
-      (and (is-pair (exp 0)) (= ((get exp 0) 0) 'unquote-splice)) ['concat ((get exp 0) 1) (handle-quasi (subvec exp 1))]
+      (= (exp 0) 'unqt) (exp 1)
+      (and (is-pair (exp 0)) (= ((get exp 0) 0) 'unqt-splice)) ['concat ((get exp 0) 1) (handle-quasi (subvec exp 1))]
       :else ['cons (handle-quasi (exp 0)) (handle-quasi (subvec exp 1))])
     ['quote exp]))
 
@@ -70,9 +71,9 @@
     ((some-fn number? boolean? fn? nil? #(contains? #{'if 'begin 'lambda 'define} %)) exp) exp
     (symbol? exp) (env-find exp env)
     (some #(= (get exp 0) %) ['set! 'define]) (def envs (assoc-in envs [env (exp 1)] (evaluate (exp 2) env)))
-    (= (get exp 0) 'if) (if (evaluate (exp 1) env) (evaluate (exp 2) env) (evaluate (exp 3) env))
+    (= (get exp 0) 'if) (if (evaluate (exp 1) env) (evaluate (exp 2) env) (evaluate (get exp 3) env))
     (= (get exp 0) 'quote) (exp 1)
-    (= (get exp 0) 'quasi-quote) (evaluate (handle-quasi (exp 1)) env)
+    (= (get exp 0) 'q-quote) (evaluate (handle-quasi (exp 1)) env)
     (= (get exp 0) 'begin) (last (map #(evaluate % env) (subvec exp 1)))
     (= (get exp 0) 'lambda) (handle-lambda exp env)
     (= (get exp 0) 'define-macro) (def macros (assoc macros (exp 1) (evaluate (exp 2) env)))
@@ -81,19 +82,19 @@
     (fn? (get exp 0)) ((get exp 0) (map #(evaluate % env) (subvec exp 1)))
     :else (if (coll? exp) exp (throw-error))))
 
-(defn parse [s eval?]
+(defn reader [s eval?]
   (cond
     (re-find #"^\)" s) (throw-error)
-    (and eval? (re-find #"^\(\s*(?:lambda|quote|quasi-quote|define-macro)\s+" s)) (let [[res rmn] (parse s false)] [(evaluate res :g) (trim rmn)])
+    (and eval? (re-find #"^\(\s*(?:lambda|quote|q-quote|define-macro|if|begin)\s+" s)) (let [[res rmn] (reader s false)] [(evaluate res :g) (trim rmn)])
     (re-find #"^\(" s) (loop [rst (trim (replace-first s #"^\(" "")), exp []]
                          (cond
                            (empty? rst) (throw-error)
                            (re-find #"^\)" rst) [(if eval? (evaluate exp :g) exp) (trim (replace-first rst #"^\)" ""))]
-                           :else (when-let [[res rmn] (parse rst eval?)] (recur (trim rmn) (conj exp res)))))
+                           :else (when-let [[res rmn] (reader rst eval?)] (recur (trim rmn) (conj exp res)))))
     :else (if-let [res (re-find #"^\S*[^\(\)\s]+" s)] [(atomize (trim res)) (trim (subs s (count res)))] s)))
 
 ;; Interpreter and REPL
-(defn interpret [s] (parse (trim s) true))
+(defn interpret [s] (reader (trim s) true))
 (defn -main []
   (do (printf "\033[0;1mcljisp ~ \u03BB  \033[34;1m") (flush)
       (let [[res rmn] (interpret (read-line))]
@@ -104,6 +105,7 @@
 ;"(define-macro when (lambda (cnd . then) (list (quote if) cnd then nil)))"
 ;"(define-macro when (lambda (cnd . then) (list (quote if) cnd (cons (quote begin) then)  nil)))"
 
+;(define-macro when (lambda (cnd . then) (quasi-quote (if (unquote cnd) (begin (unquote-splice then)) nil))))
 
 ;(define z (lambda (k l . els) (print k l els)))
 
@@ -112,4 +114,3 @@
 
 ;(define-macro my-or (lambda (x y) `(if ,x ,x ,y)))
 
-;(define-macro my-or (lambda (x y) (quasi-quote (if (unquote x) (unquote x) (unquote y)))))
